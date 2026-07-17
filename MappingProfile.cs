9bfeb233f378b4ebf8128b415189e2d3d0b7ca85
BIBHUTI BHUSAN PRUSTY <85398188+Bibhuti33@users.noreply.github.com>
2026-07-17 15:44:26 +0530

    Add files via upload

---
+++ MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using EmployeeManagementSystem.DTOs;
+using EmployeeManagementSystem.Entities;
+
+namespace EmployeeManagementSystem.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeDto>()
+                .ForMember(dest => dest.DepartmentName,
+                    opt => opt.MapFrom(src => src.Department != null
+                        ? src.Department.DepartmentName
+                        : ""))
+                .ReverseMap();
+
+            CreateMap<Department, DepartmentDto>().ReverseMap();
+
+            CreateMap<Attendance, AttendanceDto>().ReverseMap();
+        }
+    }
+}
