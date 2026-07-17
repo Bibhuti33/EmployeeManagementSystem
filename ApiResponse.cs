54be1504855ac9353a62233c0d11f27a17189bb8
BIBHUTI BHUSAN PRUSTY <85398188+Bibhuti33@users.noreply.github.com>
2026-07-17 13:00:52 +0530

    Add files via upload

---
+++ ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagementSystem.Common
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public T? Data { get; set; }
+    }
+}
