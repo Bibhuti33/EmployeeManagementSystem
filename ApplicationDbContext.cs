cdd131a6be7cc402edb43fc6f27529b6e6751c38
BIBHUTI BHUSAN PRUSTY <85398188+Bibhuti33@users.noreply.github.com>
2026-07-17 13:02:12 +0530

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeManagementSystem.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace EmployeeManagementSystem.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees => Set<Employee>();
+
+        public DbSet<Department> Departments => Set<Department>();
+
+        public DbSet<Attendance> Attendances => Set<Attendance>();
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Department)
+                .WithMany(d => d.Employees)
+                .HasForeignKey(e => e.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Attendance>()
+                .HasOne(a => a.Employee)
+                .WithMany(e => e.Attendances)
+                .HasForeignKey(a => a.EmployeeId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
