0933fd269c7973b953a53c0bd5aff0021c23cbfd
BIBHUTI BHUSAN PRUSTY <85398188+Bibhuti33@users.noreply.github.com>
2026-07-17 15:45:56 +0530

    Add files via upload

---
+++ EmployeeValidator.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagementSystem.DTOs;
+using FluentValidation;
+
+namespace EmployeeManagementSystem.Validators
+{
+    public class EmployeeValidator : AbstractValidator<EmployeeDto>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(x => x.EmployeeCode)
+                .NotEmpty()
+                .MaximumLength(20);
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Phone)
+                .NotEmpty();
+
+            RuleFor(x => x.DepartmentId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.Salary)
+                .GreaterThan(0);
+        }
+    }
+}
