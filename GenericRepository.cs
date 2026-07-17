6c6d735d031899976084cec4bdd84b92bdb88008
BIBHUTI BHUSAN PRUSTY <85398188+Bibhuti33@users.noreply.github.com>
2026-07-17 15:44:59 +0530

    Add files via upload

---
+++ GenericRepository.cs
@@ -0,0 +1,94 @@
+﻿using EmployeeManagementSystem.Context;
+using EmployeeManagementSystem.Entities;
+using EmployeeManagementSystem.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EmployeeManagementSystem.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            if (typeof(T) == typeof(Employee))
+            {
+                return (IEnumerable<T>)await _context.Employees
+                    .Include(x => x.Department)
+                    .AsNoTracking()
+                    .ToListAsync();
+            }
+
+            return await _dbSet
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            if (typeof(T) == typeof(Employee))
+            {
+                return (T?)(object?)await _context.Employees
+                    .Include(x => x.Department)
+                    .FirstOrDefaultAsync(x => x.EmployeeId == id);
+            }
+
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet
+                .Where(predicate)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.AnyAsync(predicate);
+        }
+
+        public async Task<IEnumerable<T>> GetPagedAsync(int page, int pageSize)
+        {
+            if (typeof(T) == typeof(Employee))
+            {
+                return (IEnumerable<T>)await _context.Employees
+                    .Include(x => x.Department)
+                    .AsNoTracking()
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+            }
+
+            return await _dbSet
+                .AsNoTracking()
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+    }
+}
---
+++ UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeManagementSystem.Context;
+using EmployeeManagementSystem.Entities;
+using EmployeeManagementSystem.Interfaces;
+
+namespace EmployeeManagementSystem.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+
+            Employees = new GenericRepository<Employee>(_context);
+
+            Departments = new GenericRepository<Department>(_context);
+
+            Attendances = new GenericRepository<Attendance>(_context);
+
+            Users = new GenericRepository<User>(_context);
+        }
+
+        public IGenericRepository<Employee> Employees { get; }
+
+        public IGenericRepository<Department> Departments { get; }
+
+        public IGenericRepository<Attendance> Attendances { get; }
+
+        public IGenericRepository<User> Users { get; }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
